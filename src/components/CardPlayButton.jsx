07eef26b889b5b29f6ec9a7f5749d7a3bf16b976
Lara <lara.bautista16@gmail.com>
2025-06-05 16:09:04 +0200

    add play button in playlist view

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,6 +1,6 @@
 import { Pause, Play } from "./Player"
 import { usePlayerStore } from "@/store/playerStore"
-export function CardPlayButton({ id }) {
+export function CardPlayButton({ id, size = 'small' }) {
     const {
         currentMusic,
         isPlaying,
@@ -29,9 +29,11 @@ export function CardPlayButton({ id }) {
 
     }
 
+    const iconClassName = size == 'small' ? 'w-4 h-4' : 'w-5 h-5'
+
     return (
-        <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
-            {isPlayingPlaylist ? <Pause /> : <Play />}
+        <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 hover:scale-105 transition hover:bg-green-400 p-4">
+            {isPlayingPlaylist ? <Pause className={iconClassName} /> : <Play className={iconClassName} />}
         </button>
     )
-}
+} 
